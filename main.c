ba92dac07f3e5911a9b18cf472a196bd7bcbd3e1
mr-rjh3 <mrrjh3@gmail.com>
2022-07-19 01:05:36 -0400

    Added some code to main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int *argc, char *argv[]){
+    printf("hello this is a test program\n");
+}
